373831dbca02d8a852fa55df66743edeeea99e51
Daniel Kowalski <daniel@Desktop18012000.local>
2017-04-11 19:03:27 +0200

    First config

--- desktop/src/me/daniel/game/desktop/CurvesCardDesktop.java
+++ desktop/src/me/daniel/game/desktop/CurvesCardDesktop.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import me.daniel.game.CurvesCardGame;
 
-public class DesktopLauncher {
+public class CurvesCardDesktop {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.width = CurvesCardGame.WIDTH;
+		config.height = CurvesCardGame.HEIGHT;
+		config.resizable = false;
+		
 		new LwjglApplication(new CurvesCardGame(), config);
 	}
 }
